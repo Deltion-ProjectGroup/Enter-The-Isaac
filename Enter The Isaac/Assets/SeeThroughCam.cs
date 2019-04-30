67089a1d2f4964ce59c0b3c45c5a9076f8b96511
CaspervanderKaaij <33858977+CaspervanderKaaij@users.noreply.github.com>
2019-04-30 17:44:20 +0200

    See through walls and unity settings
    See player through walls, wow
    Some settings for building

---
+++ Enter The Isaac/Assets/SeeThroughCam.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeeThroughCam : MonoBehaviour
+{
+
+    [SerializeField] List<Transform> objects;
+    Camera cam;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        cam.enabled = false;
+        for (int i = 0; i < objects.Count; i++)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, objects[i].position - transform.position, out hit, Vector3.Distance(transform.position, objects[i].position), ~LayerMask.GetMask("SpecialRender")) == true)
+            {
+                cam.enabled = true;
+            }
+        }
+    }
+}
