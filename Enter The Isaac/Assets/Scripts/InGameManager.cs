295fbcfbd32e301c85928190111e4337553e7aae
Maurits Heetkamp <33858972+MauritsHeetkamp@users.noreply.github.com>
2019-05-11 00:34:10 +0200

    Update
    Fixed a few small things
    Also added teleportation to the next level

--- Enter The Isaac/Assets/Scripts/InGameManager.cs
+++ Enter The Isaac/Assets/Scripts/InGameManager.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InGameManager : MonoBehaviour
 {
+    public static int floor = 0;
     public GameObject localPlayer;
     public DungeonCreator dungeonCreator;
     public GameObject playerPrefab;
+    public string nextLevel;
     // Start is called before the first frame update
     void Start()
     {
+        floor++;
         dungeonCreator.onGenerationComplete = SpawnPlayer;
         dungeonCreator.GenerateDungeon();
     }
@@ -17,5 +21,13 @@ public class InGameManager : MonoBehaviour
     public void SpawnPlayer()
     {
         localPlayer = Instantiate(playerPrefab, dungeonCreator.startRoom.GetComponent<StartRoom>().spawnPoint.position, dungeonCreator.startRoom.GetComponent<StartRoom>().spawnPoint.rotation);
+    }
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(nextLevel);
+    }
+    public void ResetGame()
+    {
+        floor = 0;
     }
 }
---
+++ Enter The Isaac/Assets/Scripts/Interactables/Other/Teleporter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider hit)
+    {
+        if(hit.transform.tag == "Player")
+        {
+            InGameManager inGameManager = GameObject.FindGameObjectWithTag("Manager").GetComponent<InGameManager>();
+            inGameManager.LoadNextLevel();
+        }
+    }
+}
