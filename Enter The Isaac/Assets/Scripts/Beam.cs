c7c947618da62afd5688c8675c313dac31ff68b1
ThomasBliekendaal <33858982+ThomasBliekendaal@users.noreply.github.com>
2019-06-11 14:10:37 +0200

    musicc en heel eveel shit

---
+++ Enter The Isaac/Assets/Scripts/Beam.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beam : MonoBehaviour {
+
+    [ColorUsage(true, true)]
+    public Color color;
+
+    public float speed;
+
+    public Material material;
+
+    void Start ()
+    {
+        material.SetColor("_TintColor", color);
+	}
+	
+	void Update ()
+    {
+        material.SetColor("_TintColor", color);
+
+        Vector2 offset = material.GetTextureOffset("_MainTex");
+        offset.x -= speed * Time.deltaTime;
+        material.SetTextureOffset("_MainTex", offset);
+	}
+}
