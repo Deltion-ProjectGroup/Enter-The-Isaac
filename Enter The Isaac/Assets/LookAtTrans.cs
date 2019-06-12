d4ae9620c67781779fdf721ff0768e377a763ddb
CaspervanderKaaij <33858977+CaspervanderKaaij@users.noreply.github.com>
2019-06-12 13:17:10 +0200

    Spotlights
    woah

---
+++ Enter The Isaac/Assets/LookAtTrans.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtTrans : MonoBehaviour
+{
+    [SerializeField] Transform lookAtTrans;
+    [SerializeField] float rotSpeed = 5;
+    [SerializeField] Vector3 offsetV3 = Vector3.zero;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(lookAtTrans.position - transform.position,Vector3.up) * Quaternion.Euler(offsetV3),Time.deltaTime * rotSpeed);
+    }
+}
