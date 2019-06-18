78b24645a084cf6c43a1f3d60e06aa3014d7c177
CaspervanderKaaij <33858977+CaspervanderKaaij@users.noreply.github.com>
2019-06-18 09:42:41 +0200

    Update IKHoldGun script

--- Enter The Isaac/Assets/Scripts/IKHoldGun.cs
+++ Enter The Isaac/Assets/Scripts/IKHoldGun.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IKHoldGun : MonoBehaviour
-{
+public class IKHoldGun : MonoBehaviour {
 
     Animator anim;
     [SerializeField] Transform leftHandPos;
@@ -14,71 +13,57 @@ public class IKHoldGun : MonoBehaviour
     [SerializeField] float rotateBelly;
     [SerializeField] bool findHandsByName = true;
 
+    void Start () {
+        anim = GetComponent<Animator> ();
+    }
 
-    void Start()
-    {
-        anim = GetComponent<Animator>();
-        if (findHandsByName == true)
-        {
-            InvokeRepeating("FindHands", 0, 0.01f);
+    void LateUpdate () {
+        if (findHandsByName == true) {
+            FindHands ();
         }
     }
-    void FindHands()
-    {
-        leftHandPos = null;
-        rightHandPos = null;
-        if (GameObject.Find(leftHandName))
-        {
-            leftHandPos = GameObject.Find(leftHandName).transform;
-        }
-        if (GameObject.Find(rightHandName))
-        {
-            rightHandPos = GameObject.Find(rightHandName).transform;
-        }
-        //also find belly thing
-        if (GameObject.Find(bellyName))
-        {
-            rotateBelly = GameObject.Find(bellyName).transform.localEulerAngles.y;
-        }
-        else
-        {
-            rotateBelly = 0;
+    void FindHands () {
+        if (leftHandPos == null || rightHandPos == null) {
+            leftHandPos = null;
+            rightHandPos = null;
+            if (GameObject.Find (leftHandName)) {
+                leftHandPos = GameObject.Find (leftHandName).transform;
+            }
+            if (GameObject.Find (rightHandName)) {
+                rightHandPos = GameObject.Find (rightHandName).transform;
+            }
+            //also find belly thing
+            if (GameObject.Find (bellyName)) {
+                rotateBelly = GameObject.Find (bellyName).transform.localEulerAngles.y;
+            } else {
+                rotateBelly = 0;
+            }
         }
     }
-    void OnAnimatorIK()
-    {
-        if (anim == null)
-        {
-            anim = GetComponent<Animator>();
+    void OnAnimatorIK () {
+        if (anim == null) {
+            anim = GetComponent<Animator> ();
         }
-        if (leftHandPos != null)
-        {
-            anim.SetIKPosition(AvatarIKGoal.LeftHand, leftHandPos.position);
-            anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
-        }
-        else
-        {
-            anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, 0);
+        if (leftHandPos != null) {
+            anim.SetIKPosition (AvatarIKGoal.LeftHand, leftHandPos.position);
+            anim.SetIKPositionWeight (AvatarIKGoal.LeftHand, 1);
+        } else {
+            anim.SetIKPositionWeight (AvatarIKGoal.LeftHand, 0);
         }
 
-        if (rightHandPos != null)
-        {
-            anim.SetIKPosition(AvatarIKGoal.RightHand, rightHandPos.position);
-            anim.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
-        }
-        else
-        {
-            anim.SetIKPositionWeight(AvatarIKGoal.RightHand, 0);
+        if (rightHandPos != null) {
+            anim.SetIKPosition (AvatarIKGoal.RightHand, rightHandPos.position);
+            anim.SetIKPositionWeight (AvatarIKGoal.RightHand, 1);
+        } else {
+            anim.SetIKPositionWeight (AvatarIKGoal.RightHand, 0);
         }
 
-        anim.SetBoneLocalRotation(HumanBodyBones.Spine, Quaternion.Euler(0, rotateBelly, 0));
-
+        anim.SetBoneLocalRotation (HumanBodyBones.Spine, Quaternion.Euler (0, rotateBelly, 0));
 
         //ignore ik at animations
-        if (anim.GetCurrentAnimatorStateInfo(0).IsTag("IgnoreIK") == true)
-        {
-            anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, 0);
-            anim.SetIKPositionWeight(AvatarIKGoal.RightHand, 0);
+        if (anim.GetCurrentAnimatorStateInfo (0).IsTag ("IgnoreIK") == true) {
+            anim.SetIKPositionWeight (AvatarIKGoal.LeftHand, 0);
+            anim.SetIKPositionWeight (AvatarIKGoal.RightHand, 0);
         }
     }
 }
