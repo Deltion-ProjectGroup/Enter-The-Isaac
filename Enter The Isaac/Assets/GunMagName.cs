74164cf53ac8cebbcbfdeea2c49909afc9fa969d
CaspervanderKaaij <33858977+CaspervanderKaaij@users.noreply.github.com>
2019-06-20 14:01:30 +0200

    Little easter egg lol
    nothing

---
+++ Enter The Isaac/Assets/GunMagName.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunMagName : MonoBehaviour
+{
+
+    TextMesh mesh;
+    void Start()
+    {
+        mesh = GetComponent<TextMesh>();
+        if(PlayerPrefs.GetInt ("doomMusic") == 1){
+            mesh.text = "CUM MAGICIAN";
+        }
+    }
+}
