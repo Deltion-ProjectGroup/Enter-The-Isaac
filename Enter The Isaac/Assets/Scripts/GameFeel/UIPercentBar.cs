8980da9c6e98bbed863f7c3e5b36ebb2b57ab64a
CaspervanderKaaij <33858977+CaspervanderKaaij@users.noreply.github.com>
2019-06-21 13:46:55 +0200

    im tired, please stop asking me to do everything now
    please, just stop. Im dead. Isn't it enough?

--- Enter The Isaac/Assets/Scripts/GameFeel/UIPercentBar.cs
+++ Enter The Isaac/Assets/Scripts/GameFeel/UIPercentBar.cs
@@ -5,22 +5,31 @@ using UnityEngine.UI;
 
 public class UIPercentBar : MonoBehaviour {
 
-float maxScale;
-public float curPercent = 100;
-RectTransform rect;
-[SerializeField] float lerpSpeed = 1;
+	float maxScale;
+	public float curPercent = 100;
+	RectTransform rect;
+	[SerializeField] float lerpSpeed = 1;
+	[SerializeField] bool useWitdth = false;
 
 	void Awake () {
-		rect = GetComponent<RectTransform>();
-		maxScale = rect.localScale.x;
-	}
-	
-	void Update () {
-		curPercent = Mathf.Max(curPercent,0);
-		rect.localScale = new Vector3(Mathf.Lerp(rect.localScale.x ,maxScale * (curPercent / 100),lerpSpeed * Time.unscaledDeltaTime),rect.localScale.y,rect.localScale.z);
+		rect = GetComponent<RectTransform> ();
+		if (useWitdth == false) {
+			maxScale = rect.localScale.x;
+		} else {
+			maxScale = rect.sizeDelta.x;
+		}
 	}
 
-	public void SetPercent(float newPercent){
+	void Update () {
+		curPercent = Mathf.Max (curPercent, 0);
+		if (useWitdth == false) {
+			rect.localScale = new Vector3 (Mathf.Lerp (rect.localScale.x, maxScale * (curPercent / 100), lerpSpeed * Time.unscaledDeltaTime), rect.localScale.y, rect.localScale.z);
+		} else {
+			rect.sizeDelta = new Vector2(Mathf.MoveTowards (rect.localScale.x, maxScale * (curPercent / 100),Time.deltaTime * lerpSpeed),rect.sizeDelta.y);
+		}
+	}
+
+	public void SetPercent (float newPercent) {
 		curPercent = newPercent;
 	}
 }
